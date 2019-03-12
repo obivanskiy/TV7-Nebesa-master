5c2ef535a33a9eb3d83a68462dcae357a5e2067b
Ivan <obodovskiy.ivan@gmail.com>
2019-03-12 12:28:19 +0200

    1) Managed files 2)ParsingService.swift file created

---
+++ TV7 Nebesa/Services/ParsingService.swift
@@ -0,0 +1,9 @@
+//
+//  ParsingService.swift
+//  TV7 Nebesa
+//
+//  Created by Ivan Obodovskyi on 3/12/19.
+//  Copyright © 2019 Богдан Воробйовський. All rights reserved.
+//
+
+import Foundation
