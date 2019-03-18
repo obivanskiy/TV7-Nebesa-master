5e5b5e2c0b5cb07984bcb70738495f39c8865e00
Ivan <obodovskiy.ivan@gmail.com>
2019-03-18 16:52:19 +0200

    Finished SubCategoriesScreen
    1) Sub categories tableViewController added

---
+++ TV7 Nebesa/Controllers/CategorySeriesTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CategorySeriesTableViewController.swift
+//  TV7 Nebesa
+//
+//  Created by Ivan Obodovskyi on 3/18/19.
+//  Copyright © 2019 Богдан Воробйовський. All rights reserved.
+//
+
+import UIKit
+
+class CategorySeriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        <#code#>
+    }
+    
+    
+
+}
