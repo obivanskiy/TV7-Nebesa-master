f6cd4070d9740bbfd9df74533dc2faf5f70027f2
Ivan <obodovskiy.ivan@gmail.com>
2019-03-19 18:22:50 +0200

    Reverted changes

--- TV7 Nebesa/Network/NetworkEndpoints.swift
+++ TV7 Nebesa/Network/NetworkEndpoints.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 struct NetworkEndpoints {
+<<<<<<< HEAD
     static var baseURL: String = "https://sandbox.tv7.fi"
 
     static var categoryDataURL: String = "/nebesa/api/jed/get_tv7_category_programs/?category_id="
@@ -19,4 +20,11 @@ struct NetworkEndpoints {
     static var parentCategoriesURL: String = "/nebesa/api/jed/get_tv7_parent_categories/"
     static var subCategories: String = "/nebesa/api/jed/get_tv7_sub_categories/?parent_id="
 
+=======
+    static var baseURL: String = "https://www.nebesatv7.com/api/jed/"
+    static var categoryDataURL: String = "get_tv7_category_programs?category_id="
+    static var parentCategoriesURL: String = "get_tv7_parent_categories/"
+    static var subCategoriesURL: String = "get_tv7_sub_categories/?parent_id="
+    static var seriesInfoURL: String = "get_tv7_series_info?series_id=52761"
+>>>>>>> parent of 6fb183f... Merge branch 'dev' into TV7_CategorySeriesScreens
 }
