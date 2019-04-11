386a8fac108f4919f240f9160a215df86513e414
Богдан Воробйовський <piiiko@ukr.net>
2019-04-11 18:51:35 +0300

    Collection view for home screen

---
+++ TV7 Nebesa/TestHSVC/NEW_VC/HomeSreenControllers/HomeBaseViewController.swift
@@ -0,0 +1,107 @@
+//
+//  HomeBaseViewController.swift
+//  TV7 Nebesa
+//
+//  Created by Богдан Воробйовський on 4/11/19.
+//  Copyright © 2019 Богдан Воробйовський. All rights reserved.
+//
+
+import UIKit
+
+let reIdentifier = "HomeCell"
+
+class HomeBaseViewController: BaseHomeController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    
+    @IBOutlet weak var homePageCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupMenuBar()
+        homePageCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reIdentifier)
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    //UICollectionViewDelegateFlowLayout methods
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat
+    {
+        
+        return 2;
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat
+    {
+        
+        return 1;
+    }
+    
+    
+    //UICollectionViewDatasource methods
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reIdentifier, for: indexPath) as UICollectionViewCell
+        
+        cell.backgroundColor = self.randomColor()
+        
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width , height: view.frame.height)
+    }
+    
+    
+    // custom function to generate a random UIColor
+    func randomColor() -> UIColor{
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    //computed
+    let menuBar: MenuBar = {
+        let mb = MenuBar()
+        return mb
+    }()
+    
+    //:MARK - SETUP PRIVATE MENUBAR
+    private func setupMenuBar() {
+        
+        
+        
+        let redView = UIView()
+        redView.backgroundColor = UIColor.rgb(red: 12, green: 100, blue: 194)
+        view.addSubview(redView)
+        view.addConstraintsWithFormat(withVisualFormat: "H:|[v0]|", views: redView)
+        view.addConstraintsWithFormat(withVisualFormat: "V:[v0(50)]", views: redView)
+        
+        view.addSubview(menuBar)
+        view.addConstraintsWithFormat(withVisualFormat: "H:|[v0]|", views: menuBar)
+        view.addConstraintsWithFormat(withVisualFormat: "V:[v0(50)]", views: menuBar)
+        
+        menuBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TV7 Nebesa/TestHSVC/NEW_VC/HomeSreenControllers/HomeRecommendCell.swift
@@ -0,0 +1,16 @@
+//
+//  HomeRecommendCell.swift
+//  TV7 Nebesa
+//
+//  Created by Богдан Воробйовський on 4/11/19.
+//  Copyright © 2019 Богдан Воробйовський. All rights reserved.
+//
+
+import UIKit
+
+class HomeRecommendCell: BaseCell {
+    
+    
+    @IBOutlet weak var recommendTableView: UITableView!
+    
+}
