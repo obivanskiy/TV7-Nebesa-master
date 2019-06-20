334baf4fe6588b8d6d29fee5d62ff9eac0b70e63
Богдан Воробйовський <piiiko@ukr.net>
2019-06-20 19:43:17 +0300

    Google cast refactoring is done.

--- TV7 Nebesa/AppDelegate.swift
+++ TV7 Nebesa/AppDelegate.swift
@@ -12,25 +12,68 @@ import AVFoundation
 import Kingfisher
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GCKLoggerDelegate{
 
     var window: UIWindow?
 //    private var appID: String = "7666F269"
-    
+    let kReceiverAppID = "54B61D58"
+    let kDebugLoggingEnabled = true
     private var enableGoogleCastLogging: Bool = true
     
     
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // Set your receiver application ID.
+        let criteria = GCKDiscoveryCriteria(applicationID: kReceiverAppID)
+        let options = GCKCastOptions(discoveryCriteria: criteria)
+        options.physicalVolumeButtonsWillControlDeviceVolume = true
+        GCKCastContext.setSharedInstanceWith(options)
+
+        // Theme using GCKUIStyle.
+        let castStyle = GCKUIStyle.sharedInstance()
+        // Set the property of the desired cast widgets.
+        castStyle.castViews.deviceControl.buttonTextColor = .darkGray
+        // Refresh all currently visible views with the assigned styles.
+        castStyle.apply()
+        
+        // Enable default expanded controller.
+        GCKCastContext.sharedInstance().useDefaultExpandedMediaControls = true
+
+        // Enable logger.
+        GCKLogger.sharedInstance().delegate = self
+        
+        // Set logger filter.
+        let filter = GCKLoggerFilter()
+        filter.minimumLevel = .error
+        GCKLogger.sharedInstance().filter = filter
+        
+        // Wrap main view in the GCKUICastContainerViewController and display the mini controller.
+        let appStoryboard = UIStoryboard(name: "HomeScreen", bundle: nil)
+        let navigationController = appStoryboard.instantiateViewController(withIdentifier: "MainNavigation")
+        let castContainerVC = GCKCastContext.sharedInstance().createCastContainerController(for: navigationController)
+        castContainerVC.miniMediaControlsItemEnabled = true
+        // Color the background to match the embedded content
+        castContainerVC.view.backgroundColor = .white
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = castContainerVC
+        window?.makeKeyAndVisible()
+        
+        
+        
+        
         UITabBar.appearance().backgroundColor = UIColor(red: 12/255 , green: 100/255 , blue: 194/255 , alpha: 1) //12
         UINavigationBar.appearance().tintColor = .white
         
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setCategory(AVAudioSession.Category.playback)
-        }
-        catch {
-            print("Setting category to AVAudioSessionCategoryPlayback failed.")
-        }
+//        let audioSession = AVAudioSession.sharedInstance()
+//        do {
+//            try audioSession.setCategory(AVAudioSession.Category.playback)
+//        }
+//        catch {
+//            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+//        }
 
 
         // Restrict size and expiration for cache
@@ -46,11 +89,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
-extension AppDelegate: GCKLoggerDelegate {
-    func logMessage(_ logMessage: String, at: GCKLoggerLevel, fromFunction function: String, location: String) {
-        if enableGoogleCastLogging{
-            print("Location: \(location),  function: \(function),  - \(logMessage) ")
-        }
-    }
-}
+//extension AppDelegate: GCKLoggerDelegate {
+//    func logMessage(_ logMessage: String, at: GCKLoggerLevel, fromFunction function: String, location: String) {
+//        if enableGoogleCastLogging{
+//            print("Location: \(location),  function: \(function),  - \(logMessage) ")
+//        }
+//    }
+//}
 
--- TV7 Nebesa/Presenters/HomeScreenPresenters/VideoPlayer.swift
+++ TV7 Nebesa/Presenters/HomeScreenPresenters/VideoPlayer.swift
@@ -12,12 +12,162 @@ import AVKit
 import GoogleCast
 import SVProgressHUD
 
-class VideoPlayer: UIViewController, Castable {
+
+class VideoPlayer: UIViewController, GCKSessionManagerListener, GCKRemoteMediaClientListener, GCKRequestDelegate{
     
     
+    @IBOutlet var castVideoButton: UIButton!
+    @IBOutlet var castInstructionLabel: UILabel!
+    
+    private var castButton: GCKUICastButton!
+    private var mediaInformation: GCKMediaInformation?
+    private var sessionManager: GCKSessionManager!
+    
+
+    
+    @objc func castDeviceDidChange(notification _: Notification) {
+        if GCKCastContext.sharedInstance().castState != GCKCastState.noDevicesAvailable {
+            // Display the instructions for how to use Google Cast on the first app use.
+            //            GCKCastContext.sharedInstance().presentCastInstructionsViewControllerOnce(with: castButton)
+        }
+    }
+    
+    // MARK: Cast Actions
+    
+    func playVideoRemotely() {
+        GCKCastContext.sharedInstance().presentDefaultExpandedMediaControls()
+        
+        // Define media metadata.
+        let metadata = GCKMediaMetadata()
+        metadata.setString(self.videoTitle ?? self.videoSeriesName!, forKey: kGCKMetadataKeyTitle)
+        metadata.setString(self.videoCaption ?? "Caption",
+                           forKey: kGCKMetadataKeySubtitle)
+        metadata.addImage(GCKImage(url: URL(string: self.thumbnailUrl)!,
+                                   width: 480,
+                                   height: 360))
+        
+        
+        let mediaInfoBuilder = GCKMediaInformationBuilder(contentURL: URL(string: self.videoURLString.encodeUrl()!)!)
+        mediaInfoBuilder.streamType = GCKMediaStreamType.none
+        mediaInfoBuilder.contentType = "video/mp4"
+        mediaInfoBuilder.metadata = metadata
+        mediaInformation = mediaInfoBuilder.build()
+        
+        print(self.thumbnailUrl, self.videoURLString.encodeUrl()!)
+        
+        let mediaLoadRequestDataBuilder = GCKMediaLoadRequestDataBuilder()
+        mediaLoadRequestDataBuilder.mediaInformation = mediaInformation
+        
+        // Send a load request to the remote media client.
+        if let request = sessionManager.currentSession?.remoteMediaClient?.loadMedia(with: mediaLoadRequestDataBuilder.build()) {
+            request.delegate = self
+        }
+    }
+    
+    @IBAction func loadVideo(sender _: AnyObject) {
+        print("Load Video")
+        
+        if sessionManager.currentSession == nil {
+            print("Cast device not connected")
+            return
+        }
+        
+        playVideoRemotely()
+    }
+    
+    func showLoadVideoButton(showButton: Bool) {
+        castVideoButton.isHidden = !showButton
+        // Instructions should always be the opposite visibility of the video button.
+        castInstructionLabel.isHidden = !castVideoButton.isHidden
+    }
+    
+    // MARK: GCKSessionManagerListener
+    
+    func sessionManager(_: GCKSessionManager,
+                        didStart session: GCKSession) {
+        print("sessionManager didStartSession: \(session)")
+        
+        // Add GCKRemoteMediaClientListener.
+        session.remoteMediaClient?.add(self)
+        
+        showLoadVideoButton(showButton: true)
+    }
+    
+    func sessionManager(_: GCKSessionManager,
+                        didResumeSession session: GCKSession) {
+        print("sessionManager didResumeSession: \(session)")
+        
+        // Add GCKRemoteMediaClientListener.
+        session.remoteMediaClient?.add(self)
+        
+        showLoadVideoButton(showButton: true)
+    }
+    
+    func sessionManager(_: GCKSessionManager,
+                        didEnd session: GCKSession,
+                        withError error: Error?) {
+        print("sessionManager didEndSession: \(session)")
+        
+        // Remove GCKRemoteMediaClientListener.
+        session.remoteMediaClient?.remove(self)
+        
+        if let error = error {
+            showError(error)
+        }
+        
+        showLoadVideoButton(showButton: false)
+    }
+    
+    func sessionManager(_: GCKSessionManager,
+                        didFailToStart session: GCKSession,
+                        withError error: Error) {
+        print("sessionManager didFailToStartSessionWithError: \(session) error: \(error)")
+        
+        // Remove GCKRemoteMediaClientListener.
+        session.remoteMediaClient?.remove(self)
+        
+        showLoadVideoButton(showButton: false)
+    }
+    
+    // MARK: GCKRemoteMediaClientListener
+    
+    func remoteMediaClient(_: GCKRemoteMediaClient,
+                           didUpdate mediaStatus: GCKMediaStatus?) {
+        if let mediaStatus = mediaStatus {
+            mediaInformation = mediaStatus.mediaInformation
+        }
+    }
+    
+    // MARK: - GCKRequestDelegate
+    
+    func requestDidComplete(_ request: GCKRequest) {
+        print("request \(Int(request.requestID)) completed")
+    }
+    
+    func request(_ request: GCKRequest,
+                 didFailWithError error: GCKError) {
+        print("request \(Int(request.requestID)) didFailWithError \(error)")
+    }
+    
+    func request(_ request: GCKRequest,
+                 didAbortWith abortReason: GCKRequestAbortReason) {
+        print("request \(Int(request.requestID)) didAbortWith reason \(abortReason)")
+    }
+    
+    // MARK: Misc
+    
+    func showError(_ error: Error) {
+        let alertController = UIAlertController(title: "Error",
+                                                message: error.localizedDescription,
+                                                preferredStyle: UIAlertController.Style.alert)
+        let action = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
     
     //MARK: - Stored properties
-    private var player: Player!
+    private var player: AVPlayer?
     private var playerViewController = AVPlayerViewController()
     
     var video: Video?
@@ -27,7 +177,8 @@ class VideoPlayer: UIViewController, Castable {
             setUpVideoData()
             setUpUI()
             DispatchQueue.main.async {
-                                self.createPlayerView()
+//                                self.createPlayerView()
+                self.player(urlString: self.videoURLString)
                             }
         }
     }
@@ -37,7 +188,7 @@ class VideoPlayer: UIViewController, Castable {
         SVProgressHUD.setBackgroundColor(.white)
         SVProgressHUD.setForegroundColor(UIColor.blue)
         SVProgressHUD.setDefaultMaskType(.black)
-        SVProgressHUD.show()
+//        SVProgressHUD.show()
         ApiService.shared.requestVideoInfo { (videoData: HomeScreenProgrammeInformation) in
             self.videoData = videoData
         } 
@@ -79,11 +230,30 @@ class VideoPlayer: UIViewController, Castable {
         } else {
             setUpUI()
         }
+        // Initially hide the cast button until a session is started.
+        showLoadVideoButton(showButton: false)
+        
+        sessionManager = GCKCastContext.sharedInstance().sessionManager
+        sessionManager.add(self)
+        
+        // Add cast button.
+        castButton = GCKUICastButton(frame: CGRect(x: 0, y: 0, width: 24, height: 24))
+        
+        // Used to overwrite the theme in AppDelegate.
+        castButton.tintColor = .darkGray
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: castButton)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(castDeviceDidChange(notification:)),
+                                               name: NSNotification.Name.gckCastStateDidChange,
+                                               object: GCKCastContext.sharedInstance())
+    
     }
     
     override func viewWillAppear(_ animated: Bool) {
         guard videoID == "" else {return}
-        createPlayerView()
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -91,7 +261,7 @@ class VideoPlayer: UIViewController, Castable {
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        player.stopPlayback()
+//        player.stopPlayback()
     }
     
   
@@ -133,20 +303,31 @@ class VideoPlayer: UIViewController, Castable {
         VideoFirstBroadcastLabel.text = "Доступен с: \(broadcastDateFormatter(videoFirstBroadcast))"
         
         
-        navigationItem.rightBarButtonItem = googleCastButton
+//        navigationItem.rightBarButtonItem = googleCastButton
     }
     
     //MARK: - Player function
-    private func createPlayerView() {
-   playerViewController.videoGravity = .resizeAspectFill
-        player = Player(frame: ProgramVideoView.bounds)
-        player.mediaItem = MediaItem(name: videoTitle, about: VideoCaptionLabel.text, videoUrl: videoURLString.encodeUrl()!, thumbnailUrl: thumbnailUrl.encodeUrl()!)
-        player.initPlayerLayer()
-        
-        ProgramVideoView.addSubview(player)
-        SVProgressHUD.dismiss()
-    }
-    
+//    private func createPlayerView() {
+//   playerViewController.videoGravity = .resizeAspectFill
+//        player = Player(frame: ProgramVideoView.bounds)
+//        player.mediaItem = MediaItem(name: videoTitle, about: VideoCaptionLabel.text, videoUrl: videoURLString.encodeUrl()!, thumbnailUrl: thumbnailUrl.encodeUrl()!)
+//        player.initPlayerLayer()
+//
+//        ProgramVideoView.addSubview(player)
+//        SVProgressHUD.dismiss()
+//    }
+//    MARK: - Player function
+        private func player(urlString: String) {
+            if let  videoURL = URL(string: urlString.encodeUrl()!) {
+                self.player = AVPlayer(url: videoURL)
+                playerViewController.player = self.player
+                playerViewController.view.frame = ProgramVideoView.bounds
+                self.addChild(playerViewController)
+                ProgramVideoView.addSubview(playerViewController.view)
+                playerViewController.didMove(toParent: self)
+                playerViewController.player?.pause()
+            }
+        }
     
     //MARK: - Date formatter
     private func dateFormatter(_ dateIn: String) -> String {
@@ -170,3 +351,6 @@ class VideoPlayer: UIViewController, Castable {
     
     
 }
+
+
+
